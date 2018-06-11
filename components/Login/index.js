c37dc256b4b81212e149dce61f305b351489a541
Duncan Leo <hello@duncanleo.me>
2018-06-11 17:19:16 +0800

    add .env example and package

--- components/Login/index.js
+++ components/Login/index.js
@@ -5,13 +5,12 @@ import styles from './styles.scss';
 
 class Login extends Component {
   render() {
-    var clientID = config.GOOGLE_CLIENTID;
     return (
       <div>
-        <div className='loginBtn'>
+        <div className="loginBtn">
           <GoogleLogin
             className="g-signin2"
-            clientId={clientID}
+            clientId={process.env.GOOGLE_CLIENT_ID}
             onSuccess={(googleUser) => {this.props.onSignIn(googleUser);}}
             tag="div"
             scope="https://www.googleapis.com/auth/drive.appfolder"
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+/* eslint-disable import/first */
+
+import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+dotenv.config();
+
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('app')
+ReactDOM.render(
+  (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  ), document.getElementById('app'),
 );
-
-console.log("js working");
