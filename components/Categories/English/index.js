aa686976f4af6bb4f5eacab706ad5a121ecccb8a
Duncan Leo <hello@duncanleo.me>
2018-06-11 17:59:53 +0800

    clean up English and Korean

--- components/Categories/English/index.js
+++ components/Categories/English/index.js
@@ -1,40 +1,45 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import EnglishMovies from './EnglishMovies';
 import styles from './styles.scss';
 
 class English extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       documentary: false,
       drama: false,
       movie: false,
       variety: false
-    }
+    };
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.props.genre !== nextProps.genre) {
-      const genre = nextProps.genre;
-      var updatedState = {
+    if (this.props.genre !== nextProps.genre) {
+      const { genre } = nextProps;
+      const updatedState = {
         documentary: false,
         drama: false,
         movie: false,
-        variety: false
+        variety: false,
       };
 
       switch (genre) {
-        case "documentary": updatedState.documentary = true;
+        case 'documentary':
+          updatedState.documentary = true;
           break;
 
-        case "drama": updatedState.drama = true;
+        case 'drama':
+          updatedState.drama = true;
           break;
 
-        case "movie": updatedState.movie = true;
+        case 'movie':
+          updatedState.movie = true;
           break;
 
-        case "variety": updatedState.variety = true;
+        case 'variety':
+          updatedState.variety = true;
           break;
 
         default:
@@ -46,19 +51,20 @@ class English extends Component {
   }
 
   render() {
-    if(this.props.render)
-    {
-      return(
-        <div>
-          <h1>English</h1>
-          <EnglishMovies render={this.state.movie} />
-        </div>
-      );
-    }
-    else {
-      return <div></div>;
-    }
+    return this.props.render ? (
+      <div>
+        <h1>English</h1>
+        <EnglishMovies render={this.state.movie} />
+      </div>
+    ) : (
+      <div />
+    );
   }
 }
+
+English.propTypes = {
+  genre: PropTypes.string.isRequired,
+  render: PropTypes.bool.isRequired,
+};
 
 export default English;
--- components/Categories/Korean/index.js
+++ components/Categories/Korean/index.js
@@ -1,64 +1,70 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import KoreanMovies from './KoreanMovies';
 import styles from './styles.scss';
 
 class Korean extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       documentary: false,
       drama: false,
       movie: false,
-      variety: false
-    }
+      variety: false,
+    };
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.props.genre !== nextProps.genre) {
-      const genre = nextProps.genre;
-      var updatedState = {
+    if (this.props.genre !== nextProps.genre) {
+      const { genre } = nextProps;
+      const updatedState = {
         documentary: false,
         drama: false,
         movie: false,
-        variety: false
+        variety: false,
       };
 
       switch (genre) {
-        case "documentary": updatedState.documentary = true;
+        case 'documentary':
+          updatedState.documentary = true;
           break;
 
-        case "drama": updatedState.drama = true;
+        case 'drama':
+          updatedState.drama = true;
           break;
 
-        case "movie": updatedState.movie = true;
+        case 'movie':
+          updatedState.movie = true;
           break;
 
-        case "variety": updatedState.variety = true;
+        case 'variety':
+          updatedState.variety = true;
           break;
 
         default:
           break;
       }
-      
+
       this.setState(updatedState);
     }
   }
 
   render() {
-    if(this.props.render)
-    {
-      return(
-        <div>
-          <h1>Korean</h1>
-          <KoreanMovies render={this.state.movie} />
-        </div>
-      );
-    }
-    else {
-      return <div></div>;
-    }
+    return this.props.render ? (
+      <div>
+        <h1>Korean</h1>
+        <KoreanMovies render={this.state.movie} />
+      </div>
+    ) : (
+      <div />
+    );
   }
 }
+
+Korean.propTypes = {
+  genre: PropTypes.string.isRequired,
+  render: PropTypes.bool.isRequired,
+};
 
 export default Korean;
