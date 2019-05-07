d4e22a6c7a6fc233c722c34816812051fc535e06
Elcoms <elcomsortsort@gmail.com>
2019-05-07 23:42:14 +0800

    Add list component and add form reset after submit

---
+++ components/List/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import { withFirebase } from '../Firebase';
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+    console.log("List");
+  }
+
+  makeList = (data, index) => {
+    return (
+      <li
+      key={data.name}
+      href={data.url} >
+      {data.name}
+      </li>
+    );
+  }
+
+  render() {
+    return(
+      <div>
+        <ul>
+          {this.props.data ? this.props.data.map(this.makeList) : <h1>No shows under these tag(s)</h1>}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default withFirebase(List);
