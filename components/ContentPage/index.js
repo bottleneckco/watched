fc8d942d5916f91a7639a6527283b139fea85eb2
Elcoms <elcomsortsort@gmail.com>
2019-05-11 18:47:11 +0800

    fix tags and loading error

--- components/ContentPage/index.js
+++ components/ContentPage/index.js
@@ -14,7 +14,7 @@ class ContentPage extends Component {
     this.state = {
       user: null,
       customTagsDB: null,       // custom tags that is in database
-      selectedTags: null,      // tags that user selected
+      selectedTags: [],      // tags that user selected
       defaultTags: [
         {name: 'Comedy', color: '#FFFF00'},
         {name: 'Drama', color: '#FF7373'},
@@ -38,18 +38,17 @@ class ContentPage extends Component {
   componentDidMount() {
     this.unsubscribe = this.props.firebase.user(this.props.user.uid)
     .onSnapshot(snapshot => {
-      var newArray = [];
+      /*var newArray = [];
       var customTagsDBLength = (snapshot.data().customTags) ? snapshot.data().customTags.length : 0;
 
       for(var i=0; i < (this.state.defaultTags.length + customTagsDBLength); i++) {
         newArray.push(null);
-      }
+      }*/
 
       this.setState({
         user: snapshot.data(),
-        selectedTags: newArray,
         customTagsDB: snapshot.data().customTags ? snapshot.data().customTags : [],
-        currentList: snapshot.data().watchlist ? snapshot.data().watchlist.allShows : []
+        currentList: snapshot.data().watchlist ? snapshot.data().watchlist.allShows : 'empty'
       });
     });
   }
@@ -75,9 +74,9 @@ class ContentPage extends Component {
       updateBatch.update(userRef, { "watchlist.allShows": this.props.firebase.fieldValue.arrayUnion(newShowData) });
 
     // Update custom tag field
-    if(formCustomTags[0]) {
+    if(formCustomTags.length != 0) {
       // Filter custom tags for existing tags
-      if(this.state.customTagsDB[0] != '') {
+      if(this.state.customTagsDB.length != 0) {
         var filteredFormCustomTags = [];
         var inDB = false;
 
@@ -106,7 +105,7 @@ class ContentPage extends Component {
     }
 
     // Update all tag fields
-    if(allSubmittedTags[0]) {
+    if(allSubmittedTags.length != 0) {
       var tagName = 'NIL';
       var tagObj = {};
 
@@ -141,22 +140,17 @@ class ContentPage extends Component {
 
   handleButtonTagClick = (tags, index) => {
     var newSelectedArray = this.state.selectedTags;
+    var tagNameFound = this.state.selectedTags.find((tagName) => { return tagName === tags.name });
 
-    if(newSelectedArray.length > index) {
-      if(newSelectedArray[index] === tags.name)
-        newSelectedArray[index] = null;
-    else
-        newSelectedArray[index] = tags.name;
+    tagNameFound ?
+    newSelectedArray = this.state.selectedTags.filter((tagName) => tagName != tagNameFound)  //unselected
+    : newSelectedArray.push(tags.name);                                                       //selected
 
     console.log(newSelectedArray);
     this.setState({
       selectedTags: newSelectedArray,
-      currentList: this.getSelectedShowsData(tags.name)
+      currentList: this.getSelectedShowsData(tags.name, tagNameFound, newSelectedArray)
     });
-  }
-  else {
-   console.log("index out of newSelectedArray (ButtonTags) bounds");
-  }
 }
 
  handleChange = event => {
@@ -174,28 +168,59 @@ class ContentPage extends Component {
    this.setState({ inputData: updatedData });
  }
 
- getSelectedShowsData = (tagName) => {
-   var numOfTagsSelected = (this.state.selectedTags.filter((tag) => tag == null)).length;
-   var data = [];
+ getSelectedShowsData = (tagName, isTagUnselected, sTags) => {
+   var cList = this.state.currentList;
+   var userList = this.state.viewWatchlist ? this.state.user.watchlist : this.state.user.watched;
+   var newData = [];
 
    if(!this.state.showForm) {
-     switch (numOfTagsSelected) {
-       case 0: data = this.state.viewWatchlist ? this.state.user.watchlist.allShows : this.state.user.watched.allShows;
+     if(sTags.length > 1) {
 
+       if(isTagUnselected) {
+         newData = (userList[sTags[0]]) ? userList[sTags[0]] : [];
+
+         for(let i=1; i < sTags.length; i++) {
+           let tempList = [];
+           let currentTagShows = userList[sTags[i]] ? userList[sTags[i]] : [];
+
+           currentTagShows.forEach((show) => {
+             for(let i = 0; i < newData.length; i++) {
+               if(show.name ===  newData[i].name) {
+                 tempList.push(show);
+                 break;
+               }
+             }
+           });
+           newData = tempList;
+         }
+       }
+       else {
+         let selectedTagShows = userList[tagName] ? userList[tagName] : [];
+
+         selectedTagShows.forEach((show) => {
+           for(let i = 0; i < cList.length; i++) {
+             if(show.name === cList[i].name) {
+               newData.push(show);
+               break;
+             }
+           }
+         });
+       }
+     }
+     else {
+       switch (sTags.length) {
+         case 0: newData = userList.allShows;
          break;
 
-       case 1:
-       if(this.state.viewWatchlist)
-         data = (this.state.user.watchlist[tagName]) ? this.state.user.watchlist[tagName] : [];
-       else
-         data = (this.state.user.watched[tagName]) ? this.state.user.watched[tagName] : [];
+         case 1: newData = (userList[sTags[0]]) ? userList[sTags[0]] : [];
          break;
-       default: null
 
+         default: []
+       }
      }
    };
 
-   return data;
+   return newData;
  }
 
  toggleAddShow = () => {
@@ -208,25 +233,26 @@ class ContentPage extends Component {
  handleContentHeaderClick = (onWatchlistClick) => {
    var list = null;
    if(onWatchlistClick)
-     list = (this.state.user.watchlist) ? this.state.user.watchlist.allShows : [];
+     list = (watchlist) ? watchlist.allShows : [];
    else
-     list = (this.state.user.watched) ? this.state.user.watched.allShows : [];
+     list = (watched) ? watched.allShows : [];
 
    this.setState({
      viewWatchlist: onWatchlistClick,
-     selectedTags: this.state.selectedTags.fill(null),
      currentList: list
    });
  }
 
   render() {
+  console.log("currentList: ", this.state.currentList);
+  console.log("customTagsDB", this.state.customTagsDB);
     if(this.state.currentList === null) {
-        return <h1> loading... </h1>
+        return <h1> loading... </h1>;
     }
     else {
-      // generate custom tags' buttons
       let customTagsArray = [];
       let buttonTag = {};
+
       this.state.customTagsDB.forEach((tag) => {
         if(tag) {
           buttonTag = {
@@ -236,8 +262,9 @@ class ContentPage extends Component {
           customTagsArray.push(buttonTag);
         }
       });
+
       let tags = this.state.defaultTags.concat(customTagsArray);
-      tags = tags.filter(tag => tag != undefined)
+      tags = tags.filter(tag => tag != undefined);
 
       return (
         <div>
@@ -260,13 +287,13 @@ class ContentPage extends Component {
               onChange={this.handleChange}
               onTagClick={this.handleButtonTagClick}
               tags={tags}
-              emptyList={this.state.currentList ? false : true}
+              emptyList={this.state.currentList != 'empty' ? false : true}
               closePopup={this.toggleAddShow}
               firstSubmit={this.state.firstSubmit}
               isFormValid={this.state.inputData.isFormValid} />
             : null }
 
-          {this.state.currentList ?
+          {this.state.currentList != 'empty' ?
             <List data={this.state.currentList} />
           : <h1>Your list is empty! Please add a show.</h1> }
         </div>
